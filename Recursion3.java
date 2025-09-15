245db21dd7f3eb0f3922ab011caf1a299e13f9e1
Mohammad Danish <12imtiyaz124@gmail.com>
2025-09-15 12:37:43 +0530

    more recursion problems

--- Recursion3.java
+++ Recursion3.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 public class Recursion3 {
   // Print all permutations of a string - TC = O(n!)
   public static void printPerm(String str, String permutation) {
@@ -26,9 +29,57 @@ public class Recursion3 {
     int rightPaths = countPaths(i, j+1, n, m);
     return downPaths + rightPaths;
   }
+
+  // Place tiles of size 1xm in a floor of size nxm
+  public static int placeTiles(int n, int m) {
+    if (n == m) {
+      return 2;
+    }
+    if(n < m) {
+      return 1;
+    }
+    // vertically
+    int vertPlacements = placeTiles(n-m, m);
+    // hroizontally
+    int horPlacements = placeTiles(n-1, m);
+    return vertPlacements + horPlacements;
+  }
+
+  // Find the number of ways in which you can invite n people to your party, single or in pairs
+  public static int callGuests(int n) {
+    if(n <= 1) {
+      return 1;
+    }
+    // single
+    int ways1 = callGuests(n-1);
+    // pair
+    int ways2 = (n-1) * callGuests(n-2);
+    return ways1 + ways2;
+  }
+
+  // Print all the subsets of a set of first n natural numbers - TC = O(2^n)
+  public static void printSubset(ArrayList<Integer> subset) {
+    for(int i = 0; i < subset.size(); i++) {
+      System.out.print(subset.get(i)+" ");
+    }
+    System.out.println();
+  }
+  public static void findSubsets(int n, ArrayList<Integer> subset) {
+    if(n == 0) {
+      printSubset(subset);
+      return;
+    }
+    // add in subset
+    subset.add(n);
+    findSubsets(n-1, subset);
+
+    // don't add in subset
+    subset.remove(subset.size()-1);
+    findSubsets(n-1, subset);
+  }
   public static void main(String args[]) {
-    int n = 3, m = 3;
-    int totalPaths = countPaths(0, 0, n, m);
-    System.out.print(totalPaths);
+    int n = 3;
+    ArrayList<Integer> subset = new ArrayList<>();
+    findSubsets(n, subset);
   }
 }
